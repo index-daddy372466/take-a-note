d63d3e27d8d8d5877a59b449dae9c28f64cf9f4d
kylestech95 <kylestech95@outlook.com>
2024-03-27 09:47:03 -0400

    feat: data upload to notes list

--- public/main.js
+++ public/main.js
@@ -3,18 +3,36 @@ let listContainer = document.querySelector('.textarea-list-container')
 let textarea = document.querySelector('textarea')
 let api = window.location.origin+'/notes'
 
+// fetch data from `/notes` endpoint (server/routes.js)
+    fetch(api)
+    .then(res=>{
+        return res.json();
+    })
+    .then(data=>{
+        let arr = [...data]
+         console.log(arr)
+         arr.forEach((note,index)=>{
+            const li = document.createElement('li')
+            li.classList.add('textarea-list-container>li');
+            li.classList.add('hide-item')
+            li.textContent = `${note.notes} - ${note.timestamp}`
+            listContainer.append(li)
+         })
+         const items = document.querySelectorAll('.textarea-list-container>li')
+        // console.log(items)
+        for(let x = 0; x < items.length; x++){
+            if(items[x].classList.contains('hide-item')){
+                setTimeout(()=>{
+                    items[x].classList.remove('hide-item')
+                    items[x].classList.add('show-item')
+                },50 * (x+1))
+            }
+        }
+    })
 
-// if(window.location.pathname!=='/notes'){
 
 
-//     fetch(api)
-//     .then(res=>{
-//         return res.json();
-//     })
-//     .then(data=>{
-//         let arr = [...data]
-//          console.log(arr)
-//     })
-// }
+
+
     
 
--- server/routes.js
+++ server/routes.js
@@ -22,7 +22,21 @@ module.exports = function(app,pool){
         // insert new note into db
         const insertNote = await pool.query("insert into notepad(notes) values($1)",
         [notes])
+        // // get all fields
+        // const getFields = await pool.query('select * from notepad')
+        // const rows = getFields.rows;
+        // // send notes via json
+        // res.json(rows.map(row =>{
+        //     return {notes:row.notes,timestamp:row.timestamp}
+        // }))
 
+        // alternate ending
+        res.redirect('/')
+        })
+    app.get('/notes',async(req,res)=>{
+        // res.redirect('/')
+
+        // alternate ending
         // get all fields
         const getFields = await pool.query('select * from notepad')
         const rows = getFields.rows;
@@ -30,9 +44,6 @@ module.exports = function(app,pool){
         res.json(rows.map(row =>{
             return {notes:row.notes,timestamp:row.timestamp}
         }))
-        })
-    app.get('/notes',(req,res)=>{
-        res.redirect('/')
     })
    
        
