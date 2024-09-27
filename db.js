fcc2e0eb3d2ddd6d666535ffc8be89a94e1f7cda
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-09-27 17:20:29 -0400

    cosmetic add

--- db.js
+++ db.js
@@ -2,23 +2,23 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const { Sequelize } = require('sequelize')
 
-    const sequalize = new Sequelize(process.env.URI,
-        {dialect:"sqlite",
-         storage:"./database.sqlite",
-         logging:false,
-        })
-        sequalize.sync().then(()=>{
-            console.log('you are connected to pg')
-        }).catch(err=>console.log(err))
+    // const sequalize = new Sequelize(process.env.URI,
+    //     {dialect:"sqlite",
+    //      storage:"./database.sqlite",
+    //      logging:false,
+    //     })
+    //     sequalize.sync().then(()=>{
+    //         console.log('you are connected to pg')
+    //     }).catch(err=>console.log(err))
 const pool = new Pool({
     user: process.env.DBU,
     database: process.env.DB,
     password: process.env.PD,
     port: process.env.DBP,
     host:process.env.DBH,
-    ssl:{
-        rejectUnauthorized:false,
-    }
+    // ssl:{
+    //     rejectUnauthorized:false,
+    // }
 })
 
 // console.log(pool)
